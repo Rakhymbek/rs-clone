c94f25024a22b60081db1de390db50a5188a82d1
Rakhymbek Sagyn <sagrakha@gmail.com>
2022-08-21 01:19:08 +0600

    refactor: use MUI components in Sidebar

--- src/pages/Main/SIdebar/Sidebar.tsx
+++ src/pages/Main/SIdebar/Sidebar.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { FC } from 'react';
-import { cn } from '@bem-react/classname';
+import React from "react";
+import { FC } from "react";
+import { cn } from "@bem-react/classname";
 
-import './Sidebar.css';
-import { USER } from '../../../constants';
-import { NavLink } from 'react-router-dom';
+import "./Sidebar.css";
+import { USER } from "../../../constants";
+import { NavLink } from "react-router-dom";
+import { Box, Card, CardMedia, Typography } from "@mui/material";
 
-const cnSidebar = cn('Sidebar');
+const cnSidebar = cn("Sidebar");
 
 export type SidebarProps = {
   isVisible: boolean;
@@ -14,37 +15,44 @@ export type SidebarProps = {
 
 export const Sidebar: FC<SidebarProps> = ({ isVisible }) => {
   return (
-    <div className={cnSidebar()}>
-      <div className={cnSidebar('User')}>
-        <div className={cnSidebar('User-Name')}>{USER.name}</div>
-        <div className={cnSidebar('User-Avatar')}></div>
+    <Box className={cnSidebar()}>
+      <div className={cnSidebar("User")}>
+        <Typography className={cnSidebar("User-Name")}>{USER.name}</Typography>
+        <div className={cnSidebar("User-Avatar")}></div>
       </div>
-      <div
-        className={cnSidebar('List')}
-        style={isVisible ? { display: 'block' } : { display: 'none' }}
+      <Card
+        className={cnSidebar("List")}
+        sx={
+          isVisible
+            ? { display: "block", backgroundColor: "transparent" }
+            : { display: "none" }
+        }
       >
-        <NavLink to={'/dayplaylist'}>
-          <img
-            className={cnSidebar('Button')}
-            src="./playlist/playlist-day.png"
+        <NavLink to={"/dayplaylist"}>
+          <CardMedia
+            component="img"
+            className={cnSidebar("Button")}
+            image="./playlist/playlist-day.png"
             alt="Playlist of the day"
-          ></img>
+          />
         </NavLink>
-        <NavLink to={'/hits'}>
-          <img
-            className={cnSidebar('Button')}
-            src="./playlist/playlist-hits.png"
+        <NavLink to={"/hits"}>
+          <CardMedia
+          component="img"
+            className={cnSidebar("Button")}
+            image="./playlist/playlist-hits.png"
             alt="Playlist of hits"
-          ></img>
+          />
         </NavLink>
-        <NavLink to={'/indie'}>
-          <img
-            className={cnSidebar('Button')}
-            src="./playlist/playlist-indie.png"
+        <NavLink to={"/indie"}>
+          <CardMedia
+          component="img"
+            className={cnSidebar("Button")}
+            image="./playlist/playlist-indie.png"
             alt="Indie charge"
-          ></img>
+          />
         </NavLink>
-      </div>
-    </div>
+      </Card>
+    </Box>
   );
 };
