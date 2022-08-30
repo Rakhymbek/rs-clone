a9c2633a566c7546e5f49f50acf984f117ad1f31
Daniyar <danikman89@gmail.com>
2022-08-30 21:34:40 +0600

    feat: add fetchRegister

--- src/store/auth/auth.ts
+++ src/store/auth/auth.ts
@@ -2,12 +2,17 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from '../../axios';
 
 export const fetchAuth: any = createAsyncThunk('auth/fetchAuth', async (params) => {
-  const { data } = await axios.post('/login', params);
+  const { data } = await axios.post('/auth/login', params);
   return data;
 });
 
-export const fetchAuthMe: any = createAsyncThunk('auth/fetchAuthMe', async (params) => {
-  const { data } = await axios.post('auth/me', params);
+export const fetchRegister: any = createAsyncThunk('auth/fetchRegister', async (params) => {
+  const { data } = await axios.post('/auth/register', params);
+  return data;
+});
+
+export const fetchAuthMe: any = createAsyncThunk('auth/fetchAuthMe', async () => {
+  const { data } = await axios.get('/auth/me');
   return data;
 });
 const initialState = {
@@ -45,6 +50,18 @@ const authSlice = createSlice({
       state.data = action.payload;
     },
     [fetchAuthMe.rejected]: (state: any) => {
+      state.status = 'error';
+      state.data = null;
+    },
+    [fetchRegister.pending]: (state: any) => {
+      state.status = 'loading';
+      state.data = null;
+    },
+    [fetchRegister.fulfilled]: (state: any, action: any) => {
+      state.status = 'loaded';
+      state.data = action.payload;
+    },
+    [fetchRegister.rejected]: (state: any) => {
       state.status = 'error';
       state.data = null;
     },
