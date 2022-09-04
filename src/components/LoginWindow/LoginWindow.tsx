0af36278c5afe2671046f69031e869b6e4e24c07
Daniyar <danikman89@gmail.com>
2022-09-04 18:55:19 +0600

    refactor: add validation, error and style

--- src/components/LoginWindow/LoginWindow.tsx
+++ src/components/LoginWindow/LoginWindow.tsx
@@ -4,25 +4,26 @@ import { cn } from '@bem-react/classname';
 import { NavLink } from 'react-router-dom';
 import { Box, TextField } from '@mui/material';
 
-// import { Input } from '../Input/Input';
 import { Button } from '../Button/Button';
 import Logo from '../Logo/Logo';
 
 import { useForm } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchAuth, selectIsAuth } from '../../store/auth/auth';
+import { useAppDispatch, useAppSelector } from '../../hook';
+import { fetchLogin, selectIsAuth } from '../../store/auth/auth';
 import { Navigate } from 'react-router-dom';
 
 import './LoginWindow.css';
+import { Login } from '../../store/auth/types';
 
 const cnLoginWindow = cn('LoginWindow');
 
 export const LoginWindow: FC<{}> = () => {
-  const isAuth = useSelector(selectIsAuth);
-  const dispatch = useDispatch();
+  const isAuth = useAppSelector(selectIsAuth);
+  const dispatch = useAppDispatch();
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors },
   } = useForm({
     defaultValues: {
@@ -32,10 +33,12 @@ export const LoginWindow: FC<{}> = () => {
     mode: 'onChange',
   });
 
-  const onSubmit = async (val: any) => {
-    const data = await dispatch(fetchAuth(val));
+  const onSubmit = async (val: Login) => {
+    const data: any = await dispatch(fetchLogin(val));
     if (!data.payload) {
-      return alert('Не верный логин или пароль!');
+      const formError = { type: 'server', message: 'Неверный логин или пароль' };
+      setError('password', formError);
+      setError('email', formError);
     }
     if ('token' in data.payload) {
       window.localStorage.setItem('token', data.payload.token);
@@ -45,24 +48,41 @@ export const LoginWindow: FC<{}> = () => {
   if (isAuth) {
     return <Navigate to="/main" />;
   }
+
   return (
     <Box className={cnLoginWindow()}>
       <Logo textColor="default" />
       <form onSubmit={handleSubmit(onSubmit)}>
         <TextField
+          sx={{ height: '70px' }}
+          autoComplete="off"
           fullWidth
           variant="standard"
           label="E-Mail"
           error={Boolean(errors.email?.message)}
-          type="email"
           helperText={errors.email?.message}
-          {...register('email', { required: 'Укажите почту' })}
+          {...register('email', {
+            required: 'Укажите почту',
+            pattern: {
+              value: /^([\w.*-]+@([\w-]+\.)+[\w-]{2,3})?$/,
+              message: 'Не верный формат e-mail',
+            },
+          })}
         />
         <TextField
+          sx={{ height: '100px' }}
+          autoComplete="off"
           fullWidth
+          type="password"
           variant="standard"
           label="Пароль"
-          {...register('password', { required: 'Укажите пароль' })}
+          {...register('password', {
+            required: 'Укажите пароль',
+            pattern: {
+              value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}$/,
+              message: 'Не менее 6 символов, цифры, латинские буквы в верхнем и нижнем регистре',
+            },
+          })}
           helperText={errors.password?.message}
           error={Boolean(errors.password?.message)}
         />
