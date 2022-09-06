a61585be362f60dc45dc20bf26a0ee74c307e747
Rakhymbek <sagrakha@gmail.com>
2022-09-06 16:46:14 +0600

    feat: implement handleAddToFavourites and checkFavouritesTrack functions on player

--- src/store/trackSlice.ts
+++ src/store/trackSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, current, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { SongType } from "../types";
 import dataTracks from "../pages/Main/data.json";
 
